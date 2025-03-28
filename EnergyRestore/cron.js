2dbc83d3c3145acd4756e85d86ab70557bfa6e18
DmitrySobol1 <saibleonetwo@gmail.com>
2025-03-28 03:07:59 +0300

    27-03 cnange09

--- EnergyRestore/cron.js
+++ EnergyRestore/cron.js
@@ -1,19 +1,23 @@
 import cron from 'node-cron';
 import { executeDailyTask } from './dailyTask.js';
 
-// Запуск каждый день в 3:00 (с указанием временной зоны) 
-cron.schedule('*/3 * * * *', async () => {
-  console.log('🚀 Запуск ежедневной задачи...', new Date().toISOString());
-  
-  try {
-    await executeDailyTask();
-    console.log('✅ Задача успешно выполнена');
-  } catch (error) {
-    console.error('❌ Ошибка выполнения задачи:', error);
+// Запуск каждый день в 3:00 (с указанием временной зоны)
+cron.schedule(
+  '1 0 * * *',
+  async () => {
+    console.log('🚀 Запуск ежедневной задачи...', new Date().toISOString());
+
+    try {
+      await executeDailyTask();
+      console.log('✅ Задача успешно выполнена');
+    } catch (error) {
+      console.error('❌ Ошибка выполнения задачи:', error);
+    }
+  },
+  {
+    scheduled: true,
+    timezone: 'Europe/Moscow',
   }
-}, {
-  scheduled: true,
-  timezone: "Europe/Moscow"
-});
+);
 
 console.log('⏰ Планировщик задач инициализирован');
